6a52f13e78f9de3ce1bae0cc92c7201a84f3d818
LRezende <leonardo@lrezende.eti.br>
2016-04-15 10:03:01 -0300

    vim

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTDIR=~/dot
 ln -v -s -f ${DOTDIR}/bashrc ~/.bashrc
 ln -v -s -f ${DOTDIR}/tmux.conf ~/.tmux.conf
 ln -v -s -f ${DOTDIR}/tmux.dev.conf ~/.tmux.dev.conf
+ln -v -s -f ${DOTDIR}/vimrc ~/.vimrc
 
 [[ -d ~/bin ]] || mkdir -v ~/bin
 ln -v -s -f ${DOTDIR}/bin/tm ~/bin/tm
